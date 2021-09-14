56993259531592b6594f21f2764fabe665633895
Khanh Pham <phamduckhanh2411@gmail.com>
2021-09-14 22:46:27 +0700

    update syncservice

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { WrapperComponent } from './lib/features/component-generator/wrapper/wrapper-component';
 import { initReactSnap } from './lib/features/injection/snap';
 import * as config from './config';
+import { SyncService } from './lib/features/sync/sync-service';
 
 export {
   WrapperComponent,
   initReactSnap,
-  config
+  config,
+  SyncService
 }

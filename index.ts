bf4ed419a464d5fa41b1f2e7ba701cb81721da44
Khanh Pham <phamduckhanh2411@gmail.com>
2021-09-19 00:19:38 +0700

    fix defect of sync service

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { WrapperComponent } from './lib/features/component-generator/wrapper/wrapper-component';
 import { initReactSnap } from './lib/features/injection/snap';
-import { SyncService } from './lib/features/sync/sync-service';
 
 export {
   WrapperComponent,
-  initReactSnap,
-  SyncService
+  initReactSnap
 }
